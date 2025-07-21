c44badc4d9e92207725148f0d8ccafe0e1785f55
Mohit <bishtmohit872@gmail.com>
2025-07-21 14:38:24 +0530

    re-updating fetchsummary

--- frontend/FinTracker/src/customhooks/fetchSummary.js
+++ frontend/FinTracker/src/customhooks/fetchSummary.js
@@ -2,20 +2,24 @@ import { useEffect } from "react"
 import { useImperativeHandle } from "react"
 import { BASE_URL } from "../utils/constant"
 import { useDispatch } from "react-redux"
-import { addSummary,removeSummary } from "../utils/summarySlice" 
+import { addSummary, removeSummary } from "../utils/summarySlice"
 import axios from "axios"
 
-export const fetchSummary = ()=>{
+export const fetchSummary = () => {
     const dispatch = useDispatch()
 
-    useEffect(()=>{
-        getSummary()
-    },[]) 
+    useEffect(() => {
+        const getSummary = async () => {
+            try{
 
-    const getSummary = async ()=>{
-        const res = await axios.get(BASE_URL+"/api/transaction/summary",{withCredentials:true}) 
-        dispatch(addSummary(res?.data))
-        // console.log(res?.data)
-    }
+                const res = await axios.get(BASE_URL + "/api/transaction/summary", { withCredentials: true })
+                dispatch(addSummary(res?.data))
+            }
+            catch(err){
+                console.error('error in summary api:',err)
+            }
+        }
+        getSummary()
+    }, [])
 
 }
