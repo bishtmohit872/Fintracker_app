53fdc6070a3b519376f6a3813d36eb11798d831b
Mohit <bishtmohit872@gmail.com>
2025-07-21 13:52:16 +0530

    re-updating origin in cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config();
 
 app.use(cors({
-    origin:["http://localhost:5173","https://fintracker-app-backend.onrender.com"],
+    origin:["http://localhost:5173","https://fintracker-app-frontend.onrender.com"],
     credentials:true,
 }))
 app.use(express.json())
